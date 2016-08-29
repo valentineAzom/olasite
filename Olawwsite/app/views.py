43a922c7105d14c019507f40dfc7a01f6cbefca7
valentine azom <215041656@stu.ukzn.ac.za>
2016-08-29 06:54:13 +0200

    final

---
+++ Olawwsite/app/views.py
@@ -0,0 +1,18 @@
+﻿from django.shortcuts import render,redirect
+from django.views.generic import *
+from django.core.urlresolvers import reverse_lazy,reverse
+from datetime import datetime
+
+# Create your views here.
+def index(request):
+    year = datetime.today().year
+    return render(request,'index.html',{"year":year})
+def services(request):
+    year = datetime.today().year
+    return render(request,'services.html',{"year":year})
+def about(request):
+    year = datetime.today().year
+    return render(request,'About.html',{"year":year})
+def contact(request):
+    year = datetime.today().year
+    return render(request,'Contact.html',{"year":year})
---
+++ Olawwsite/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hp\documents\visual studio 2013\Projects\Olawwsite\Olawwsite\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
